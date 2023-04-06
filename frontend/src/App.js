7ebd428e4f4bc44a14fc5df1cd60cd680293001d
cathqine <122431966+cathqine@users.noreply.github.com>
2023-04-06 20:00:35 +1000

    code from MUI and website to make cards

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import RestaurantCard from "./components/RestaurantCard";
 
 function App() {
   return (
@@ -18,6 +19,10 @@ function App() {
           Learn React
         </a>
       </header>
+      {/* scroll down to see the restaurant card example: */}
+      <RestaurantCard>
+      </RestaurantCard>
+
     </div>
   );
 }
