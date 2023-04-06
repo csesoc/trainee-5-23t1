765bfd09575e4d3b4653ae1c672c8157fdf4eb94
cathqine <122431966+cathqine@users.noreply.github.com>
2023-04-06 20:42:27 +1000

    use of centering: bad layout after 1st row

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
       {/* scroll down to see the restaurant card example: */}
       {/* note a maximum of 4 cards per stack, excluding any border requirements */}
       
-      <Stack direction="row" spacing={2}>
+      <Stack direction="row" justifyContent="center" spacing={2}>
         <RestaurantCard></RestaurantCard>
         <RestaurantCard></RestaurantCard>
         <RestaurantCard></RestaurantCard>
@@ -17,7 +17,7 @@ function App() {
       </Stack>
       {/* can't find a way to have spacing between rows w/o using <br></br> */}
       <br></br> 
-      <Stack direction="row" spacing={2}>
+      <Stack direction="row" justifyContent="center" spacing={2}>
         <RestaurantCard></RestaurantCard>
         <RestaurantCard></RestaurantCard>
         <RestaurantCard></RestaurantCard>
