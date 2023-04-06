48eb7b04240ca8671d089b29a1e53edb56e4fd99
cathqine <122431966+cathqine@users.noreply.github.com>
2023-04-06 20:32:52 +1000

    delete header to have a pg full of cards

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,20 +6,6 @@ import Stack from '@mui/joy/Stack';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       {/* scroll down to see the restaurant card example: */}
       {/* note a maximum of 4 cards per stack, excluding any border requirements */}
       
@@ -37,6 +23,8 @@ function App() {
         <RestaurantCard></RestaurantCard>
         <RestaurantCard></RestaurantCard>
       </Stack>
+
+
     </div>
   );
 }
