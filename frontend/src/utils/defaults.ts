853a9753f5d236317cc19b5a827818b82199efd9
Jo Wu <jo.wu@student.unsw.edu.au>
2023-05-26 03:50:28 +1000

    added google maps url field to restaurant storage type

--- frontend/src/utils/defaults.ts
+++ frontend/src/utils/defaults.ts
@@ -1,36 +1,38 @@
 export type RestaurantEleType = {
-  image?: string,
-  text?: string
-}
+  image?: string;
+  text?: string;
+};
 
 export type RestaurantStorageType = {
-  name: string,
-  image: string,
-  location: string,
-  embed: string,
-  priceRange: string,
-  rating: number,
+  name: string;
+  image: string;
+  location: string;
+  googleMapsUrl: string;
+  embed: string;
+  priceRange: string;
+  rating: number;
   tags: {
-    suburb: string,
-    cuisine: string,
-    other: Array<string>
-  },
-  descriptions: string,
-  elements: Array<RestaurantEleType>
+    suburb: string;
+    cuisine: string;
+    other: Array<string>;
+  };
+  descriptions: string;
+  elements: Array<RestaurantEleType>;
 };
 
 export const restaurantDefault: RestaurantStorageType = {
   name: "",
   image: "",
   location: "",
+  googleMapsUrl: "",
   embed: "",
   priceRange: "",
   rating: 0,
   tags: {
     suburb: "",
     cuisine: "",
-    other: []
+    other: [],
   },
   descriptions: "",
-  elements: []
+  elements: [],
 };
